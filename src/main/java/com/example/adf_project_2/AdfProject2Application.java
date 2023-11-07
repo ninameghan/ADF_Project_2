9dcc7da768ed061c18bf37c03c96977097c0928c
lemmenn <nina.lemmens@hva.nl>
2023-11-07 14:44:44 +0100

    Initial commit

---
+++ src/main/java/com/example/adf_project_2/AdfProject2Application.java
@@ -0,0 +1,13 @@
+package com.example.adf_project_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdfProject2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdfProject2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/adf_project_2/AdfProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adf_project_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdfProject2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
